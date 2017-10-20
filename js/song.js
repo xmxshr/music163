d6d1d53a56b843b42e26f2f20c1c494469fae354
xmxshr <xmxshr@me.com>
2017-10-20 15:49:20 +0800

    1. 将获取的歌词数据添加到页面上 2. 增加翻译歌词 并修改相应的样式

--- js/song.js
+++ js/song.js
@@ -8,8 +8,12 @@ $(function () {
     video.src = song.songUrl
     video.oncanplay = function () {
       video.play()
-      $('.play-button .icon-play').css({display: 'none'})
-      $('.play-button .icon-pause').css({display: 'block'})
+      $('.play-button .icon-play').css({
+        display: 'none'
+      })
+      $('.play-button .icon-pause').css({
+        display: 'block'
+      })
       $('.disc-container .disc').addClass('active')
     }
     video.onended = function () {
@@ -17,16 +21,21 @@ $(function () {
     }
     bindEventButton(video)
     createSongInfo(song)
-    getLyric(song, video)
+
+
+    getLyric(song.lyric, video)
+    if (song.tlyric) {
+      getTranslateLyric(song.tlyric)
+
+    }
   })
 
-  function getLyric(song, video) {
-    let lyric = song.lyric
+
+  function getTranslateLyric(lyric) {
+    let lyrics = lyric
     let regex = /\[(.+)\](.+)/
     let array = lyric.split(/\\n/)
     array.pop()
-    console.log(array)
-    
     array = array.map(function (string, index) {
       let matches = string.match(regex)
       if (matches) {
@@ -39,40 +48,86 @@ $(function () {
         }
       }
     })
-    array = array.filter(function(x){
-      if(x !== undefined){return x}
+    array = array.filter(function (x) {
+      if (x !== undefined) {
+        return x
+      }
     })
-    console.log(array)
-    setLyric(array, video)
+    let $lyrics = $('.song-description .lyric .words p')
+    for (let i = 0; i < array.length; i++) {
+      let span = `<span class='tlyric'>${array[i].lyric}</span>`
+      for (let j = 0; j < $lyrics.length; j++) {
+        if (+$lyrics.eq(j).attr('data-time') === array[i].time) {
+          $lyrics.eq(j).append(span)
+          $lyrics.eq(j).addClass('addTlyric')
+          break
+        }
+      }
+    }
+    $('.song-description .lyric .words .tlyric').css({
+      fontSize: '14px',
+      lineHeight: '14px',
+      display: 'block'
+    })
   }
 
+  function getLyric(lyric, video) {
+    let lyrics = lyric
+    let regex = /\[(.+)\](.+)/
+    let array = lyric.split(/\\n/)
+    array.pop()
+    array = array.map(function (string, index) {
+      let matches = string.match(regex)
+      if (matches) {
+        let regexTime = /(\d+):(.+)/
+        let minutes = +matches[1].match(regexTime)[1]
+        let seconds = +matches[1].match(regexTime)[2]
+        return {
+          time: minutes * 60 + seconds,
+          lyric: matches[2]
+        }
+      }
+    })
+    array = array.filter(function (x) {
+      if (x !== undefined) {
+        return x
+      }
+    })
+
+    let $lyricCt = $('.song-description .lyric .words')
+    for (let i = 0; i < array.length; i++) {
+      let p = `<p data-time=${array[i].time}>${array[i].lyric}</p>`
+      $lyricCt.append(p)
+    }
+
+    setLyric(array, video)
+
+  }
 
   function setLyric(array, video) {
-    setInterval(function(){
+    setInterval(function () {
       let currentTime = video.currentTime
-      // createLyric(array)
-      for(let i=0; i<array.length; i++){
-          if(i === array.length-1){
-            console.log(array[i].lyric)
-          }else if(array[i].time <= currentTime && currentTime < array[i+1].time){
-            console.log(array[i].lyric)
-            break
-          }
+      let $lyrics = $('.song-description .lyric .words p')
+      let $whichLine
+      for (let i = 0; i < array.length; i++) {
+        if (i === array.length - 1) {
+          $whichLine = $lyrics.eq(i)
+        } else if ($lyrics.eq(i).attr('data-time') <= currentTime && currentTime < $lyrics.eq(i + 1).attr('data-time')) {
+          $whichLine = $lyrics.eq(i)
+          break
         }
-        
-
+      }
+      if ($whichLine) {
+        $whichLine.addClass('active').prev().removeClass('active')
+        let top = $whichLine.offset().top
+        let ctTop = $('.song-description .lyric .words').offset().top
+        let delta = top - ctTop - $('.song-description .lyric').innerHeight() / 3
+        $('.song-description .lyric .words').css({
+          transform: `translateY(-${delta}px)`
+        })
+      }
     }, 300)
-    
   }
-
-  function createLyric(node){
-    let p = `<p>${node.lyric}</p>`
-    $('.song-description .lyric .word').append(p)
-    $('.song-description .lyric .word').css({
-      transform: 'translateY(-32px)'
-    })
-  }
-
 
   function createSongInfo(song) {
     let h2 = `<span>${song.name}</span>
@@ -86,33 +141,26 @@ $(function () {
     })
   }
 
-
-  //   let $lyric = $('.lyric .words')
-  //   array.map(function(object){
-  //     if(!object){return}
-  //     let $p = $('<p></p>')
-  //     $p.attr('data-time', object.time).text(object.words)
-  //     $lyric.append($p)
-  //   })
-  // })
-
-
-
-
-
-
-  function bindEventButton(video){
+  function bindEventButton(video) {
     $('.play-button').on('click', function () {
       let $playButton = $('.play-button .icon-play')
       let $pauseButton = $('.play-button .icon-pause')
       if ($playButton.css('display') === 'block') {
-        $playButton.css({display: 'none'})
-        $pauseButton.css({display: 'block'})
+        $playButton.css({
+          display: 'none'
+        })
+        $pauseButton.css({
+          display: 'block'
+        })
         video.play()
         $('.disc-container .disc').addClass('active')
       } else {
-        $playButton.css({display: 'block'})
-        $pauseButton.css({display: 'none'})
+        $playButton.css({
+          display: 'block'
+        })
+        $pauseButton.css({
+          display: 'none'
+        })
         video.pause()
         $('.disc-container .disc').removeClass('active')
       }
@@ -120,6 +168,3 @@ $(function () {
   }
 
 })
-
-
-// play() pause()  onplay()  oncanplay() currentTime
